e3b3011a6186b10898cc81160d55a57899a7e978
NoaJacquet <noajacquet2004@gmail.com>
2024-02-18 12:13:32 +0100

    feat: ajout du systeme de note

--- style/musicLector.js
+++ style/musicLector.js
@@ -1,5 +1,5 @@
-const audio = document.querySelector('audio');
-const totalTime = document.getElementById('total-time');
+const audio = document.querySelector("audio");
+const totalTime = document.getElementById("total-time");
 const lector = document.getElementById("lector");
 const soundBar = document.getElementById("sound-bar");
 const soundVolume = document.getElementById("sound-volume");
@@ -11,94 +11,49 @@ const creerPlaylist = document.getElementById("creer-playlist");
 const musique = document.getElementById("son");
 
 totalTime.textContent = buildDuration(audio.duration);
-lector.setAttribute("max", audio.duration)
+lector.setAttribute("max", audio.duration);
 musicName.textContent = audio.src;
 
 function buildDuration(duration) {
-    let min = Math.floor(duration / 60);
-    let sec = String(Math.floor(duration % 60)).padStart(2,'0');
-    return min +':' + sec;
+  let min = Math.floor(duration / 60);
+  let sec = String(Math.floor(duration % 60)).padStart(2, "0");
+  return min + ":" + sec;
 }
 
-playButton.addEventListener("click", function(){
-    audio.play();
-    playButton.style.display = "none";
-    pauseButton.style.display = "initial";
+playButton.addEventListener("click", function () {
+  audio.play();
+  playButton.style.display = "none";
+  pauseButton.style.display = "initial";
 });
 
-musique.addEventListener("click", function(){
-    audio.play();
-    playButton.style.display = "none";
-    pauseButton.style.display = "initial";
+musique.addEventListener("click", function () {
+  audio.play();
+  playButton.style.display = "none";
+  pauseButton.style.display = "initial";
 });
 
-pauseButton.addEventListener("click", function(){
-    audio.pause();
-    playButton.style.display = "initial";
-    pauseButton.style.display = "none";
+pauseButton.addEventListener("click", function () {
+  audio.pause();
+  playButton.style.display = "initial";
+  pauseButton.style.display = "none";
 });
 
-audio.addEventListener("timeupdate", function(){
-    progressTime.textContent = buildDuration(this.currentTime);
-    lector.value = this.currentTime;
-    audio.volume = soundBar.value /100;
+audio.addEventListener("timeupdate", function () {
+  progressTime.textContent = buildDuration(this.currentTime);
+  lector.value = this.currentTime;
+  audio.volume = soundBar.value / 100;
 });
 
-lector.addEventListener("input", function(){
-    audio.currentTime = this.value;
-    progressTime.textContent = buildDuration(this.value);
+lector.addEventListener("input", function () {
+  audio.currentTime = this.value;
+  progressTime.textContent = buildDuration(this.value);
 });
 
-soundBar.addEventListener("input", function(){
-    soundVolume.textContent = this.value + "%";
+soundBar.addEventListener("input", function () {
+  soundVolume.textContent = this.value + "%";
 });
 
-$(document).ready(function(){
-    var trigger = $('main #playlist ul li '),
-    container = $('#main');
-    trigger.on('click', function(){
-        // $.ajax({
-        //     url: 'template/Accueil.php',
-        //     type: 'POST',
-        //     data: 'template/Playlist.php',
-        //     dataType: 'php', // Spécifie le type de données attendu
-        //     success: function(response) {
-        //         $('#main').html(response);
-        //     },
-        //     error: function(xhr, status, error) {
-        //         // En cas d'erreur, afficher un message d'erreur
-        //         console.error('Erreur lors du chargement du contenu :', status, error);
-        //     }
-        // });
-        //container.load("template/PlaylistDetail.php");
-    });
-});
-
-// $(document).ready(function(){
-//     var trigger = $('header img'),
-//     container = $('#main');
-//     trigger.on('click', function(){
-//         container.load("template/Playlist.php");
-//     });
-// });
-
-// $(document).ready(function(){
-//     var trigger = $('#main #genre'),
-//     container = $('#main');
-//     trigger.on('click', function(){
-//         container.load("template/Genre.php");
-//     });
-// });
-
-// $(document).ready(function(){
-//     var trigger = $('#main #album'),
-//     container = $('#main');
-//     trigger.on('click', function(){
-//         container.load("template/Album.php");
-//     });
-// });
-
-creerPlaylist.addEventListener("click", function(){
-    const playlistName = prompt('Entrez le nom de la playlist :');
-    console.log(playlistName);
+creerPlaylist.addEventListener("click", function () {
+  const playlistName = prompt("Entrez le nom de la playlist :");
+  console.log(playlistName);
 });
--- template/album_detail.php
+++ template/album_detail.php
@@ -6,6 +6,7 @@
     <title>Accueil</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://unpkg.com/htmx.org@latest/dist/htmx.js"></script>
+    <script src="../style/note.js" defer></script>
 </head>
 <body>
     <?php
@@ -26,15 +27,44 @@
             use modele_bd\MusiqueBD;
             use modele_bd\AlbumBD;
             use modele_bd\ArtistesBD;
-    
+            use modele_bd\NoterBD;
+            use modele\Noter;
+            
             $connexion = new Connexion();
             $connexion->connexionBD();
-    
+
+            $noteManager = new NoterBD($connexion->getPDO());
             $musiqueManager = new MusiqueBD($connexion->getPDO());
             $artisteManager = new ArtistesBD($connexion->getPDO());
             $albumManager = new AlbumBD($connexion->getPDO());
             echo "<h1>".$albumManager->getAlbumById($musiqueId)->getTitreAlbum()." par ".$artisteManager->getArtistByAlbumId($musiqueId)->getNomArtistes()."</h1>";
-
+            ?> 
+            <div>
+                <?php
+                echo "<p>Moyenne des notes de l'album : ".$noteManager->calculerMoyenneNotesParAlbum($musiqueId)."/5</p>";
+                echo "<p>Votre note : ".$noteManager->getNoteAlbumById(1, $musiqueId)."</p>";
+                ?>
+                <i class="star">&#9733</i>
+                <i class="star">&#9733</i>
+                <i class="star">&#9733</i>
+                <i class="star">&#9733</i>
+                <i class="star">&#9733</i>
+                <form id="myForm" method="POST">
+                    <input type="hidden" id="hiddenInput" name="hiddenInput">
+                    <label for="hiddenInput">Votre note:</label>
+                    <input id="valider" type="submit" value="Validez" name="btn-valider">
+                </form>
+                <?php
+                // Vérifier si le formulaire a été soumis
+                if(isset($_POST['btn-valider'])) {
+                    // Afficher le texte en PHP lorsque le bouton "submit" est cliqué
+                    $note = $_POST['hiddenInput'];
+                    $notation = new Noter(1, $musiqueId, $note);
+                    $noteManager->insertNote($notation);
+                }
+                ?>
+            </div>
+            <?php
             $musiques = $musiqueManager->getMusiquesByAlbumId($musiqueId);
             foreach($musiques as $key => $musique){
                 echo "<li>";
@@ -58,4 +88,5 @@
 </div>
 </body>
 <script src="../style/musicLector.js"></script>
+
 </html>
